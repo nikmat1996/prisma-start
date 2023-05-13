b259c5657089dec6779689d47fa4dd77e6fe0aee
nikmat1996 <nikhilmathew1996@gmail.com>
2023-05-13 15:07:59 +0530

    init

---
+++ script.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'Mathew'
+        }
+    })
+
+    console.log(user)
+}
+
+main()
+    .catch(e => {
+    console.log(e.message)
+})
+    .finally(async () => {
+    await prisma.$disconnect()
+})
